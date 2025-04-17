6fcc68e908abbe40716c403a275331cab257148e
SDallasMacLellanTrevor <maclellan.trevor@gmail.com>
2025-04-17 12:18:57 -0400

    Added basic login feature and updated DB
    Added a rudimentary login feature on the employee page and updated the employee table in the database.

--- app/app/routes/web.php
+++ app/app/routes/web.php
@@ -1,23 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return redirect()->to('/login/employees')->send();
 });
-
 Route::get('/login', function () {
     return redirect()->to('/login/employees')->send();
 });
 
+
 Route::get('/login/employees', function () {
-    return view('login');
+    return view('employeeLogin');
+});
+Route::post('/login/employees', function (Request $request) {
+
+    $input_name = $request->input("username");
+    $employee = DB::table("employees")->where("employee_id", $input_name)->first();
+    if (isset($employee)) {
+        return redirect()->to('/login/employees')->send();  // MAKE THIS REDIRECT TO THE MAIN PAGE WHEN FINISHED
+    } else {
+    return view('employeeLogin');
+    }
 });
 
-Route::post('/login/employees', function () {
-    return view('login');
-});
 
 Route::get('/login/managers', function () {
     return view('managerLogin');
 });
+Route::post('/login/managers', function () {
+    return view('managerLogin');
+});
+
