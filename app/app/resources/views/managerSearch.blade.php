232fc8ac8caee0d343e856d3346b395e2b626570
SDallasMacLellanTrevor <maclellan.trevor@gmail.com>
2025-04-20 10:47:36 -0400

    Added new icons and updated form format
    Added some icons for the form as well as placeholder redirects, and updated the form format and added some placeholder data.

--- app/app/resources/views/managerSearch.blade.php
+++ app/app/resources/views/managerSearch.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=add_circle" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
     <title>Document</title>
-
     <style>
-
-</style>
-
+        tbody tr:nth-child(odd) {
+            background-color:#adadad;
+        }
+    </style>
 </head>
 
 <body style="margin:0px; height:100%; background-color:#d9d9d9; font-family: Helvetica, Sans-Serif;">
@@ -23,15 +23,15 @@
 
     <div style="height:10%"></div>
 
-    <div style="text-align:center;">
+    <div style="text-align:center; height:60%; overflow-y:scroll; overflow-x:hidden; scrollbar-width:none; font-weight:bold;">
         <table style="margin-left:auto; margin-right:auto; border-collapse:collapse; width:900px; text-align:left;">
             <thead>
                 <tr style="border:1px solid black;">
-                    <td style="padding-left:10px; width:10vw">Product Code</td>
-                    <td style="padding:10px;">Product Name</td>
+                    <td style="padding-left:10px; padding:10px; width:10vw">Product Code</td>
+                    <td>Product Name</td>
                     <td>Category</td>
                     <td>Tags</td>
-                    <td style="width:3vw; text-align:center">
+                    <td style="width:3vw; text-align:left">
                         <a>     <!-- MAKE THIS REDIRECT TO THE "ADD PRODUCT" PAGE WHENEVER THAT GETS FINISHED -->
                             <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
                                 add_circle
@@ -40,6 +40,80 @@
                     </td>
                 </tr>
             </thead>
+            <tbody>
+                <tr style="border:1px solid black;">
+                    <td style="padding-left:10px; padding:10px; width:10vw">00001</td>
+                    <td>Product 1</td>
+                    <td>Category</td>
+                    <td>Tag, Tag</td>
+                    <td style="width:3vw; text-align:left">
+                        <a>     <!-- MAKE THIS REDIRECT TO THE "EDIT" PAGE WHENEVER THAT GETS FINISHED -->
+                            <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
+                                edit
+                            </span>
+                        </a>
+                        <a>     <!-- MAKE THIS CALL WHATEVER DELETE FUNCTION I'VE IMPLEMENTED LATER -->
+                            <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
+                                delete
+                            </span>
+                        </a>
+                    </td>
+                </tr>
+                <tr style="border:1px solid black;">
+                    <td style="padding-left:10px; padding:10px; width:10vw">00002</td>
+                    <td>Product 2</td>
+                    <td>Category</td>
+                    <td>Tag, Tag</td>
+                    <td style="width:3vw; text-align:left">
+                        <a>     <!-- MAKE THIS REDIRECT TO THE "EDIT" PAGE WHENEVER THAT GETS FINISHED -->
+                            <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
+                                edit
+                            </span>
+                        </a>
+                        <a>     <!-- MAKE THIS CALL WHATEVER DELETE FUNCTION I'VE IMPLEMENTED LATER -->
+                            <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
+                                delete
+                            </span>
+                        </a>
+                    </td>
+                </tr>
+                <tr style="border:1px solid black;">
+                    <td style="padding-left:10px; padding:10px; width:10vw">00003</td>
+                    <td>Product 3</td>
+                    <td>Category</td>
+                    <td>Tag, Tag</td>
+                    <td style="width:3vw; text-align:left">
+                        <a>     <!-- MAKE THIS REDIRECT TO THE "EDIT" PAGE WHENEVER THAT GETS FINISHED -->
+                            <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
+                                edit
+                            </span>
+                        </a>
+                        <a>     <!-- MAKE THIS CALL WHATEVER DELETE FUNCTION I'VE IMPLEMENTED LATER -->
+                            <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
+                                delete
+                            </span>
+                        </a>
+                    </td>
+                </tr>
+                <tr style="border:1px solid black;">
+                    <td style="padding-left:10px; padding:10px; width:10vw">00004</td>
+                    <td>Product 4</td>
+                    <td>Category</td>
+                    <td>Tag, Tag</td>
+                    <td style="width:3vw; text-align:left">
+                        <a>     <!-- MAKE THIS REDIRECT TO THE "EDIT" PAGE WHENEVER THAT GETS FINISHED -->
+                            <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
+                                edit
+                            </span>
+                        </a>
+                        <a>     <!-- MAKE THIS CALL WHATEVER DELETE FUNCTION I'VE IMPLEMENTED LATER -->
+                            <span class="material-symbols-outlined" style="font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24">
+                                delete
+                            </span>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </div>
 </body>
